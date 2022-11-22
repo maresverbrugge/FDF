85f2fd924760283102d27cf580ea25c0282e93ab
Mares Verbrugge <maresverbrugge@gmail.com>
2022-11-22 15:09:41 +0100

    Reading to start drawing things

--- src/fill_data.c
+++ src/fill_data.c
@@ -6,7 +6,7 @@
 /*   By: mverbrug <mverbrug@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/10 15:58:56 by mverbrug      #+#    #+#                 */
-/*   Updated: 2022/11/22 12:41:37 by mverbrug      ########   odam.nl         */
+/*   Updated: 2022/11/22 15:02:12 by mverbrug      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,45 +34,113 @@ double	degree_to_rad(double degree)
 	return (degree * (M_PI / 180));
 }
 
-void	edit_data_points(t_map *map_data)
+
+void	rotation_on_z_axis(t_map *map_data)
 {
 	int	i;
-	int previous_x;
-    int previous_y;
-	int	z;
 	double rad;
+	int old_x;
+    int old_y;
+	int	z;
 
 	i = 0;
-	rad = degree_to_rad(30.0);
+	rad = degree_to_rad(45.0);
 	printf("rad = %f\n", rad);
 	while (i < map_data->amount_of_points)
 	{
-		previous_x = map_data->data_points[i].x;
-		previous_y = map_data->data_points[i].y;
+		old_x = map_data->data_points[i].x;
+		old_y = map_data->data_points[i].y;
 		z = map_data->data_points[i].z;
-		// map_data->data_points[i].x = ((previous_x - previous_y) * cos(rad))* 10;
-		// map_data->data_points[i].y = (-z + (previous_x + previous_y) * sin(rad)) * 10;
-
-		map_data->data_points[i].x = ((previous_x * cos(rad)) - (previous_y * sin(rad)))* 10;
-		map_data->data_points[i].y = (-z + (previous_x * sin(rad)) + (previous_y * cos(rad)))* 10;
-
-		// map_data->data_points[i].x = (i % map_data->columns) * 42;
-		// printf("x = %i\t", map_data->data_points[i].x);
-		// map_data->data_points[i].y = (i / map_data->columns) * 42;
-		// printf("y = %i\t", map_data->data_points[i].y);
-		// map_data->data_points[i].z = map_data->int_array[i];
-		// printf("z = %i\t\n", map_data->data_points[i].z);
+		map_data->data_points[i].x = ((old_x * cos(rad)) - (old_y * sin(rad)));
+		map_data->data_points[i].y = ((old_x * sin(rad)) + (old_y * cos(rad)));
 		i++;
 	}
-
-
-	// #include <math.h>
-	// int hoek = 65 * (M_PI / 180.0);
-	// float old_x = 5.0;
-	// float old_y = 5.0;
-	// float new_x = old_x * cos(hoek) - old_y * sin(hoek);
-	// float new_y = old_x * sin(hoek) + old_y * cos(hoek);
-	// ft_printf("x = %d\n", new_x);
-	// ft_printf("y = %d\n", new_y);
 }
+
+void	rotation_on_x_axis(t_map *map_data)
+{
+	int	i;
+	double rad;
+    int old_y;
+	int	old_z;
+
+	i = 0;
+	rad = degree_to_rad(45.0);
+	printf("rad = %f\n", rad);
+	while (i < map_data->amount_of_points)
+	{
+		old_y = map_data->data_points[i].y;
+		old_z = map_data->data_points[i].z;
+		map_data->data_points[i].y = ((old_y * cos(rad)) - (old_z * sin(rad)));
+		map_data->data_points[i].z = ((old_y * sin(rad)) + (old_z * cos(rad)));
+		i++;
+	}
+}
+
+void	rotation_on_y_axis(t_map *map_data)
+{
+	int	i;
+	double rad;
+    int old_x;
+	int	old_z;
+
+	i = 0;
+	rad = degree_to_rad(45.0);
+	printf("rad = %f\n", rad);
+	while (i < map_data->amount_of_points)
+	{
+		old_x = map_data->data_points[i].x;
+		old_z = map_data->data_points[i].z;
+		map_data->data_points[i].x = ((old_x * cos(rad)) + (old_z * sin(rad)));
+		map_data->data_points[i].z = ((-old_x * sin(rad)) + (old_z * cos(rad)));
+		i++;
+	}
+}
+
+void	edit_data_points(t_map *map_data)
+{
+	int	i;
+	// int old_x;
+    // int old_y;
+	// int	z;
+	double rad;
+
+	i = 0;
+	rad = degree_to_rad(45.0);
+	printf("rad = %f\n", rad);
+	printf("map_data rows = %d\n", map_data->rows);
+
+	while (i < map_data->amount_of_points)
+	{
+		map_data->data_points[i].x *= 10;
+		map_data->data_points[i].y *= 10;
+		// old_x = map_data->data_points[i].x;
+		// old_y = map_data->data_points[i].y;
+		// z = map_data->data_points[i].z;
+		// map_data->data_points[i].x = ((old_x - old_y) * cos(rad))* 10;
+		// map_data->data_points[i].y = (-z + (old_x + old_y) * sin(rad)) * 10;
+		// OF:
+		// map_data->data_points[i].x = ((old_x * cos(rad)) - (old_y * sin(rad))) * 10;
+		// map_data->data_points[i].y = (-z + (old_x * sin(rad)) + (old_y * cos(rad))) * 10;
+		// OF:
+		// map_data->data_points[i].x = ((old_x - old_y) * cos(rad))* 10;
+		// map_data->data_points[i].y = ((old_x * sin(rad)) + (old_y * cos(rad)));
+		// map_data->data_points[i].y = ((map_data->data_points[i].y * cos(rad)) - (z * sin(rad)))* 10;
+		i++;
+	}
+	rotation_on_z_axis(map_data);
+	rotation_on_x_axis(map_data);
+	// rotation_on_y_axis(map_data);
+}
+
+
+
+// #include <math.h>
+// int hoek = 65 * (M_PI / 180.0);
+// float old_x = 5.0;
+// float old_y = 5.0;
+// float new_x = old_x * cos(hoek) - old_y * sin(hoek);
+// float new_y = old_x * sin(hoek) + old_y * cos(hoek);
+// ft_printf("x = %d\n", new_x);
+// ft_printf("y = %d\n", new_y);
 
