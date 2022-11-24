cdea271aa38974341334c5418505642cf065c2de
Mares Verbrugge <maresverbrugge@gmail.com>
2022-11-24 09:14:47 +0100

    Reading to start drawing things

--- src/draw_grid.c
+++ src/draw_grid.c
@@ -6,7 +6,7 @@
 /*   By: mverbrug <mverbrug@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/17 18:06:33 by mverbrug      #+#    #+#                 */
-/*   Updated: 2022/11/22 17:07:55 by mverbrug      ########   odam.nl         */
+/*   Updated: 2022/11/24 09:14:42 by mverbrug      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void	draw_vertical(mlx_image_t *the_map, t_map *map_data)
 	}
 }
 
-void	draw_diagonal(mlx_image_t *the_map, t_map *map_data)
+void	bresenham(mlx_image_t *the_map, t_map *map_data)
 {
 	int	i;
 	int	start_x;
@@ -90,7 +90,7 @@ void	draw_grid(mlx_image_t *the_map, t_map *map_data)
 	i = 0;
 	draw_horizontal(the_map, map_data);
 	draw_vertical(the_map, map_data);
-	draw_diagonal(the_map, map_data);
+	// bresenham(the_map, map_data);
 	while (i < map_data->amount_of_points)
 	{
 		mlx_put_pixel(the_map, 100 + map_data->data_points[i].x,
